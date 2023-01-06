4ce55493e215ae5dfafb6e59ab6b254feb04d8c4
João Paulo Barbosa Anastácio <89879181+Mamede5151@users.noreply.github.com>
2023-01-06 16:01:13 -0300

    Primeiro teste

---
+++ demo/src/test/java/com/CasaDoCodigo/demo/ProjetoLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CasaDoCodigo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
